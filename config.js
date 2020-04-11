f5c81269107447921f115074c11ac8df49f2d880
javierreraul <758906@unizar.es>
2020-04-11 17:38:45 +0200

    MongoDB problems

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
   port : process.env.PORT || 3000,
-  db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/shop',
+  db: process.env.ATLAS_URI || 'mongodb://localhost:27017/shop',
   SECRET_TOKEN: 's3cr3t_t0k3n'
 }
