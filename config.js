5cc040839ef228d9c3c3625dc6b7f068cf7871f9
javierreraul <758906@unizar.es>
2020-04-11 17:16:29 +0200

    automatic_deploy

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 module.exports = {
   port : process.env.PORT || 3000,
   db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/shop',
-  SECRET_TOKEN: 'miclavedetokens'
+  SECRET_TOKEN: 's3cr3t_t0k3n'
 }
